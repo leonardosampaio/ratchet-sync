d56d4741315b6f8270c5f321bf3d74c5d5d04376
Leonardo Sampaio <leonardosamp@live.com>
2022-02-25 02:23:53 -0300

    service

--- systemd/ratchet.sh
+++ systemd/ratchet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup /usr/bin/php /usr/share/ratchet-sync/cli/server.php /usr/share/ratchet-sync/configuration/websocket.json & >> /var/log/ratchet.log 2>&1
+nohup /usr/bin/php /usr/share/ratchet-sync/cli/server.php /usr/share/ratchet-sync/configuration/websocket.json
