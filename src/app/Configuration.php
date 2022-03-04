b3e8358a6b509ab7ad7af9f4eb13459f721e0dea
Leonardo Sampaio <leonardosamp@live.com>
2022-03-04 01:34:25 -0300

    renewing token

--- src/app/Configuration.php
+++ src/app/Configuration.php
@@ -21,11 +21,56 @@ class Configuration {
     
     public function getApplication()
     {
-        return $this->getJsonFromConfigFile(__DIR__.'/../../configuration/app.json');
+        $configObj = $this->getJsonFromConfigFile(__DIR__.'/../../configuration/app.json');
+
+        if ($configObj->token &&
+            $configObj->token->expiredDate &&
+            $configObj->login && 
+            $configObj->password && 
+            $configObj->token->expiredDate - time()*1000 < 0)
+        {
+            $url = '/gms/application/login';
+
+            $objPayload = [
+                'login' => $configObj->login,
+                'password' => $configObj->password
+            ];
+
+            $result = (new CurlWrapper())->post(
+                $configObj->baseUrl . $url,
+                [],
+                json_encode($objPayload),
+                [
+                    'Content-Type: application/json',
+                    'Accept: application/json'
+                ],
+                $configObj->port ?? 443
+            );
+        
+            if (201 === $result['httpCode'])
+            {
+                $response = json_decode($result['response']);
+
+                $configObj->token = [
+                    'value' => $response->token,
+                    'expiredDate' => $response->expiredDate
+                ];
+
+                file_put_contents(__DIR__.'/../../configuration/app.json',
+                    json_encode($configObj, JSON_PRETTY_PRINT));
+            }
+        }
+
+        return $configObj;
     }
 
     public function updateCredentials($login, $password, $token, $expiredDate)
     {
+        if (!$login || !$password || !$token || !$expiredDate)
+        {
+            return;
+        }
+
         $applicationConfig = $this->getApplication();
 
         if (isset($applicationConfig->error))
@@ -41,6 +86,7 @@ class Configuration {
         $applicationConfig->login = $login;
         $applicationConfig->password = $password;
 
-        file_put_contents(__DIR__.'/../../configuration/app.json', json_encode($applicationConfig, JSON_PRETTY_PRINT));
+        file_put_contents(__DIR__.'/../../configuration/app.json',
+            json_encode($applicationConfig, JSON_PRETTY_PRINT));
     }
 }
